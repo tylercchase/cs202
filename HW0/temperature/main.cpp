7c7daea46df5261bde3cc1a192a82de977232968
tylercchase <tylercchase@gmail.com>
2020-01-14 17:06:23 -0900

    Implement function math

--- HW0/temperature/main.cpp
+++ HW0/temperature/main.cpp
@@ -3,29 +3,32 @@
 using std::cout;
 using std::endl;
 
-
+//Cpp method for farenheit to celsius
 double cpp_ftoc(const char* str){
-    return 0;
+    return std::stod(str) - 32 * 5 / 9;
 }
+
+//C method for string to double with celsius to farenheits
 double c_ctof(const char* str){
-    return 0;
+    char* end;
+    return std::strtod(str, &end) * 9 / 5 + 32;
+    
 }
 
 int main(int argc, char* argv[]){
     //check if arguments supplied match required length
-    for(int x=0; x < argc; x++){
-        cout << x << ": " << argv[x] << endl;
-    }
     if(argc == 3){
         if( strcmp(argv[1],"--ftoc") == 0){
-            cout << cpp_ftoc(argv[3]) << endl;
+            cout << cpp_ftoc(argv[2]) << endl;
         }
-        else if(strcmp(argv[1],"--ctof")){
-            cout << c_ctof(argv[3]) << endl;
-        }else{
+        else if(strcmp(argv[1],"--ctof") == 0){
+            cout << c_ctof(argv[2]) << endl;
+        }
+        else{
             cout << "Invalid argument" << argv[1] << endl;
         }
-    }else{
+    }
+    else{
         cout << "Invalid number of arguments" << endl;
     }
     return 0;
