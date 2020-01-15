51929d6bbb71c4f8805f3628fe694e99cf92e915
tylercchase <tylercchase@gmail.com>
2020-01-14 16:31:18 -0900

    Take commnd line arguments

---
+++ HW0/temperature/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+
+double cpp_ftoc(const char* str){
+    return 0;
+}
+double c_ctof(const char* str){
+    return 0;
+}
+
+int main(int argc, char* argv[]){
+    cout << argv[2] << endl;
+    cout << "Hello World" << endl;
+    return 0;
+}
