f74fe0e7d5a7268b2161e8c2dc9c9d0877847cd2
tylercchase <tylercchase@gmail.com>
2020-01-18 17:16:53 -0900

    Add func to get total lines in file

--- HW0/gutenberg/main.cpp
+++ HW0/gutenberg/main.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <regex>
+#include <time.h>
 
 bool download(int number, int type){
     std::string command;
     auto newType = type + 1;
     if(type == 0){
-    command = "wget -qO output.txt http://www.gutenberg.org/files/" + std::to_string(number) + "/" + std::to_string(number) + ".txt 2>/dev/null";
+        command = "wget -qO output.txt http://www.gutenberg.org/files/" + std::to_string(number) + "/" + std::to_string(number) + ".txt 2>/dev/null";
     }
     else if(type == 1){
-    command = "wget -qO output.txt http://www.gutenberg.org/files/" + std::to_string(number) + "/" + std::to_string(number) + "-0.txt 2>/dev/null";
-
+        command = "wget -qO output.txt http://www.gutenberg.org/files/" + std::to_string(number) + "/" + std::to_string(number) + "-0.txt 2>/dev/null";
     }
     else if(type == 2){
-    command = "wget -qO output.txt http://www.gutenberg.org/files/" + std::to_string(number) + "/pg" + std::to_string(number) + "-0.txt 2>/dev/null";
+        command = "wget -qO output.txt http://www.gutenberg.org/files/" + std::to_string(number) + "/pg" + std::to_string(number) + "-0.txt 2>/dev/null";
     }
     system(command.c_str());
     std::string line;
     std::ifstream myfile ("output.txt");
+
+    //Check if file is null and if it is try a different URL
     if (myfile.is_open())
     {
         std::string line;
         getline (myfile,line);
         std::cout << line << std::endl;
+        myfile.close();
+
         if(line != ""){
             return true;
         }
@@ -39,8 +42,26 @@ bool download(int number, int type){
     return false;
 }
 
+int getNumLines(){
+    int numLines = 0;
+    std::string line;
+    std::ifstream myfile("output.txt");
+
+    while (std::getline(myfile, line))
+        ++numLines;
+    return numLines;
+}
+
 
 int main(){
-    download(250,0);
+    if(download(250,0)){
+        std::cout << "Succesful download" << std::endl;
+    } 
+    else {
+        std::cout << "Unsuccesful download" << std::endl;
+    }
+    int numLines = getNumLines();
+    std::cout << numLines << std::endl;
+
     // std::string command = "wget -O stdout";
 }
