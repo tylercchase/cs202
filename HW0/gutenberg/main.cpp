f0ed1a1f736339fe00353d5b528957a35d247e69
tylercchase <tylercchase@gmail.com>
2020-01-18 16:57:06 -0900

    Pipe errors to /dev/null

--- HW0/gutenberg/main.cpp
+++ HW0/gutenberg/main.cpp
@@ -7,14 +7,14 @@ bool download(int number, int type){
     std::string command;
     auto newType = type + 1;
     if(type == 0){
-    command = "wget  -fO output.txt http://www.gutenberg.org/files/" + std::to_string(number) + "/" + std::to_string(number) + ".txt";
+    command = "wget -qO output.txt http://www.gutenberg.org/files/" + std::to_string(number) + "/" + std::to_string(number) + ".txt 2>/dev/null";
     }
     else if(type == 1){
-    command = "wget -fO output.txt http://www.gutenberg.org/files/" + std::to_string(number) + "/" + std::to_string(number) + "-0.txt";
+    command = "wget -qO output.txt http://www.gutenberg.org/files/" + std::to_string(number) + "/" + std::to_string(number) + "-0.txt 2>/dev/null";
 
     }
     else if(type == 2){
-    command = "wget -f -O output.txt http://www.gutenberg.org/files/" + std::to_string(number) + "/pg" + std::to_string(number) + "-0.txt";
+    command = "wget -qO output.txt http://www.gutenberg.org/files/" + std::to_string(number) + "/pg" + std::to_string(number) + "-0.txt 2>/dev/null";
     }
     system(command.c_str());
     std::string line;
@@ -24,7 +24,7 @@ bool download(int number, int type){
         std::string line;
         getline (myfile,line);
         std::cout << line << std::endl;
-        if(line == "\n"){
+        if(line != ""){
             return true;
         }
         else if(type == 2){
@@ -41,6 +41,6 @@ bool download(int number, int type){
 
 
 int main(){
-    download(200,0);
+    download(250,0);
     // std::string command = "wget -O stdout";
 }
