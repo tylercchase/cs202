da26daf3c06f8cf362802286e4ffefa9b9019fb3
tylercchase <tylercchase@gmail.com>
2020-01-27 15:15:45 -0900

    Time searching of vector

--- HW1/timeit1/main.cpp
+++ HW1/timeit1/main.cpp
@@ -4,38 +4,41 @@
 #include "StopWatch.hpp"
 
 std::vector<int> gen_group(int amount){
+    std::random_device rd;
+    std::mt19937 gen(rd());
     std::vector<int> output;
+    std::uniform_int_distribution<> dis(1,1000000000);
+
     for(int x=0; x < amount; x++){
-        output.push_back(x);
+        output.push_back(dis(gen));
     }
+    std::cout << "Done creating; first num:" << output[0] << std::endl;
+    std::sort(output.begin(),output.end());
+    std::cout << "Done sorting; first num:" << output[0] << std::endl;
+
     return output;
 }
-void run_test(){
 
-}
 
 int main(){
 
     std::random_device rd;
     std::mt19937 gen(rd());
     StopWatch timer;
-    std::vector<int> amounts{1,2,3,4,5};
+    std::vector<int> amounts{10,100,1000,10000,1000000,1000000,1000000000};
+
     for(int i = 0; i < amounts.size(); i++){
-
-    std::uniform_int_distribution<> dis(1,amounts[i]);
-    auto stuff = gen_group(amounts[i]);
-    std::cout << "Running search 5 times on dataset of size: " << amounts[i] << std::endl;
-    for(int x = 0; x < 5; x++){
-        auto find = dis(gen);
-        timer.start();
-        std::search(stuff.begin(),stuff.end(),find);
-        timer.end();
-        std::cout << timer.get_time_milsec() << std::endl;
+        std::uniform_int_distribution<> dis(1,amounts[i]);
+        auto stuff = gen_group(amounts[i]);
+        std::cout << "Running search 5 times on dataset of size: " << amounts[i] << std::endl;
+        for(int x = 0; x < 5; x++){
+            std::vector<int> find{stuff[dis(gen)]};
+            timer.start();
+            std::search(stuff.begin(),stuff.end(),find.begin(), find.end());
+            timer.end();
+            std::cout  << timer.get_time_milsec() << std::endl;
+        }
     }
-    }
-    
     timer.end();
-    std::cout << timer.get_time_milsec();
-
-
+    return 0;
 }
