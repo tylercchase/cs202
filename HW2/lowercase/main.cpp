e29915341ed6cb21fa8f1ca3fb895bdab27d9009
tylercchase <tylercchase@gmail.com>
2020-02-02 21:06:27 -0900

    Add basic func

---
+++ HW2/lowercase/main.cpp
@@ -0,0 +1,28 @@
+#define CATCH_CONFIG_MAIN
+#include <iostream>
+#include "catch.hpp"
+int count_char(char* input){
+    int counter = 0;
+
+    while(input[counter] != '\0'){
+        counter++;
+    }
+    return counter;
+}
+
+void to_lower(char *s){
+    int counter = count_char(s);
+    for (int i = 0; i < counter; i++){
+        if(*(s+i) >= 65 && *(s+i) <= 90){
+            *(s+i) = (*(s+i)+40);
+        }
+    }
+    std::cout << counter << *(s+1) << std::endl;
+}
+
+TEST_CASE("Lowercase test","[lowercase]"){
+    char* test = (char*)"Hello World!";
+    to_lower(test);
+    std::cout << test << std::endl;
+    REQUIRE(test == "hello world!");
+}
