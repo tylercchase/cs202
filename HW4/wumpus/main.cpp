32412d0800ca2c23575461c86777f1e4e525c5ea
tylercchase <tylercchase@gmail.com>
2020-03-03 16:27:08 -0900

    Skeleton of save system

--- HW4/wumpus/main.cpp
+++ HW4/wumpus/main.cpp
@@ -1,22 +1,43 @@
 #include <iostream>
 #include <fstream>
-
+#include <vector>
+#include <string>
+#include <map>
 struct room {
+    int roomNumber;
     int roomType;
     //roomType contains either
+    //
     // 0 - Empty room
     // 1 - Room with a wumpus
     // 2 - Room with a pit
     // 3 - Room with a bat
     int connectedRooms[3];
 };
-void initializeCaveSystem(){
+std::map<int,room> initializeCaveSystem(){
+    std::ifstream saveFile;
+    saveFile.open("save.txt");
+    std::map<int,room> rooms;
+
+    std::string roomLine;
+    while(getline(saveFile,roomLine,' ')){
+        std::cout << roomLine << std::endl;
+    }
+}
+void makeSave(){
     std::ofstream saveFile;
     saveFile.open("save.txt");
+    int numBats{2};
+    int numWumpus{1};
+    int numPits{2};
+    int numRooms{20};
+    //Example: 10:0:12,3,4
+    for (int x = 1; x <= numRooms; x++){
 
+    }
 }
 int main(){
     std::ifstream saveFile;
-
-    return 0;
+    // initializeCaveSystem();
+    return 0;   
 }
