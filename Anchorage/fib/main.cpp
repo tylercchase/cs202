13f53872cfda4b9ec54489d935625302dea6ac3c
tylercchase <tylercchase@gmail.com>
2020-04-08 03:51:53 -0800

    Fix fib

--- Anchorage/fib/main.cpp
+++ Anchorage/fib/main.cpp
@@ -19,7 +19,7 @@ int fib_loop(int n){
         int holder1{1};
         int holder2{1};
         int sum{2};
-        for(int x =2; x < n; x++){
+        for(int x =2; x < (n-1); x++){
             holder1 = sum;
             sum = holder2 + sum;
             holder2 = holder1;
