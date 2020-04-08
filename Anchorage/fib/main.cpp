1bafc379de461e36b01ac0231206c7a95aee46a7
tylercchase <tylercchase@gmail.com>
2020-04-08 02:15:36 -0800

    Write recursive fib sequence

--- Anchorage/fib/main.cpp
+++ Anchorage/fib/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
 int fib(int n){
-    if(n <= 1){
-        return n;
+    if( n==0 ){
+        return 0;
+    }
+    if( n == 1){
+        return 1;
     }
     return fib(n-1) + fib(n-2);
 }
