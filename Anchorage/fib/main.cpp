a2a17d57a8084ea0d79d517284f919545c27df39
tylercchase <tylercchase@gmail.com>
2020-04-08 03:08:32 -0800

    Non-recursive fib

--- Anchorage/fib/main.cpp
+++ Anchorage/fib/main.cpp
@@ -10,7 +10,21 @@ int fib(int n){
     return fib(n-1) + fib(n-2);
 }
 int fib_loop(int n){
-    return 0;
+        if(n==0){
+        return 1;
+        }
+        if(n == 1){
+            return 1;
+        }
+        int holder1{1};
+        int holder2{1};
+        int sum{2};
+        for(int x =2; x < n; x++){
+            holder1 = sum;
+            sum = holder2 + sum;
+            holder2 = holder1;
+        }
+        return sum;
 }
 
 int main(){
@@ -26,7 +40,8 @@ int main(){
         std::cout << "Result: " << fib(userNumber) << std::endl;
         break;
     case 2:
-        std::cout << "Result" << fib_loop(userNumber) << std::endl;
+        std::cout << "Result: " << fib_loop(userNumber) << std::endl;
+        break;
     default:
         std::cout << "Invalid menu selection" << std::endl;
     }
