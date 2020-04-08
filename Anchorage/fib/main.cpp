faa17201fe8b9662f22dd4bdc185b6c9344bae4a
tylercchase <tylercchase@gmail.com>
2020-04-08 02:13:59 -0800

    Take user input

---
+++ Anchorage/fib/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int fib(int n){
+    if(n <= 1){
+        return n;
+    }
+    return fib(n-1) + fib(n-2);
+}
+int fib_loop(int n){
+    return 0;
+}
+
+int main(){
+    std::cout << "Enter a number to select method of finding the fibonacci  number \n 1.) Recursive \n 2.) Non-recursive" << std::endl;
+    int userChoice;
+    std::cin >> userChoice;
+    std::cout << "Enter number to find in the fibonacci sequence" << std::endl;
+    int userNumber;
+    std::cin >> userNumber;
+    switch (userChoice)
+    {
+    case 1:
+        std::cout << "Result: " << fib(userNumber) << std::endl;
+        break;
+    case 2:
+        std::cout << "Result" << fib_loop(userNumber) << std::endl;
+    default:
+        std::cout << "Invalid menu selection" << std::endl;
+    }
+    
+}
