7e8745d330605c4318772b8b52e63e038210833c
tylercchase <tylercchase@gmail.com>
2020-04-14 18:59:52 -0800

    Write setters and getters

--- Takotna/cityNode.hpp
+++ Takotna/cityNode.hpp
@@ -5,8 +5,60 @@ class CityNode {
         double longitude;
         double graphx;
         double graphy;
+
+    public:
+
     public:
         CityNode(unsigned int _number, double _latitude, double _longitude):
             number(_number), latitude(_latitude),longitude(longitude){};
-    
+        unsigned int getNumber()
+        {
+            return number;
+        }
+
+        void setNumber(int number)
+        {
+            number = number;
+        }
+
+        double getLatitude()
+        {
+            return latitude;
+        }
+
+        void setLatitude(double latitude)
+        {
+            latitude = latitude;
+        }
+
+        double getLongitude()
+        {
+            return longitude;
+        }
+
+        void setLongitude(double longitude)
+        {
+            longitude = longitude;
+        }
+
+        double getGraphx()
+        {
+            return graphx;
+        }
+
+        void setGraphx(double graphx)
+        {
+            graphx = graphx;
+        }
+
+        double getGraphy()
+        {
+            return graphy;
+        }
+
+        void setGraphy(double graphy)
+        {
+            graphy = graphy;
+        }
+
 };
