c68d44bfaf4436a505029a7ae5914c305569511f
Tyler Chase <tylercchase@gmail.com>
2020-04-30 16:43:22 -0800

    << Operator lab07

--- labs/lab07/main.cpp
+++ labs/lab07/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include "Student.hpp"
 
+std::ostream & operator<<(std::ostream & os, const Student &student){
+    // std::cout << student._gpa;
+    os << "Student stats\nGPA:" << student._gpa << "\nAge: " << student._age;
+    return os;
+}
 int main(){
     const Student student;
-    
+    std::cout << student << std::endl;
     return 0;
 }
